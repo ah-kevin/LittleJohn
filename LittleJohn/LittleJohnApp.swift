5cb3539fad8e9f78fa8a99e286e85ae4f1513923
bjke <kbjwdh@gmail.com>
2023-06-03 19:38:55 +0800

    init

---
+++ LittleJohn/LittleJohnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleJohnApp.swift
+//  LittleJohn
+//
+//  Created by bjke on 2023/6/3.
+//
+
+import SwiftUI
+
+@main
+struct LittleJohnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
